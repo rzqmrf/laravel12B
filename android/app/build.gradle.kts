ca787899836fd1ed60c35ed1a3fde308ba7b482c
rozaq <rzqmrf@gmail.com>
2026-04-03 08:50:30 +0700

    nyarr

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,13 +1,17 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    // Flutter Gradle Plugin harus setelah Android & Kotlin
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.e_reserv"
-    compileSdk = flutter.compileSdkVersion
+
+    // Paksa pakai versi terbaru
+    compileSdk = 34
+    buildToolsVersion = "34.0.0"
+
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -20,20 +24,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.e_reserv"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = flutter.minSdkVersion         // sesuaikan kebutuhan
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
